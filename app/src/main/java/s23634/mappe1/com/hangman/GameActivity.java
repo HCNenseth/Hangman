96f3af885dd942f7b6f507678e5c1411bcf48b8b
Hans Christian Nenseth <nenseth@gmail.com>
2015-09-13 23:53:34 +0200

    days end

--- app/src/main/java/s23634/mappe1/com/hangman/GameActivity.java
+++ app/src/main/java/s23634/mappe1/com/hangman/GameActivity.java
@@ -1,9 +1,29 @@
 package s23634.mappe1.com.hangman;
 
+import android.content.res.Resources;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * Created by Hans Christian on 13.09.2015.
- */
+import java.util.Random;
+
 public class GameActivity extends AppCompatActivity {
+
+    private String[] words;
+    private Random random;
+    private String current_word;
+    private LinearLayout wordLayout;
+    private TextView[] charViews;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Resources res = getResources();
+        words = res.getStringArray(R.array.dictionary);
+
+        random = new Random();
+        current_word = "";
+        wordLayout = (LinearLayout) findViewById(R.id.word);
+    }
 }
